fbe826bd0a407f8809097be44ac105d1caeb6acb
Gombos János <54068067+gombosjanos@users.noreply.github.com>
2021-12-02 12:17:05 +0100

    méret, fájltípus vizsgálat

--- upload.php
+++ upload.php
@@ -9,24 +9,30 @@ if (isset($_FILES["fileToUpload"])) {
     $target_dir = "uploads/";
 
     $i=0;
-    $errirs = array();
+    $errors = array();
+    $allowed_filetypes = array ('image/png', 'image/jpg', 'image/jpeg');
 
     foreach($_FILES["fileToUpload"]["name"] as $key => $name){
          $target_file = $target_dir . basename($name);
         
-         if($_FILES["fileToUpload"]["size"][$key] > 1024000){
-             $errors[0][] ="A $name TÚL NAGY";
+         if($_FILES["fileToUpload"]["size"][$key] > 102400){
+             $errors[$key][] ="A $name TÚL NAGY";
              
          }
-         if($_FILES["fileToUpload"]["size"][$key] < 1024){
-            $errors[0][] ="A $name TÚL KICSI";
+         elseif($_FILES["fileToUpload"]["size"][$key] < 1024){
+            $errors[$key ][]="A $name TÚL KICSI";
             
         }
+        if(!in_array($_FILES["fileToUpload"]["type"][$key], $allowed_filetypes)){
+            $errors[$key][] ="A $name nem jpg vagy png.";
+        }
+        if(!isset($errors[$key])){
          if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file)) {
             $i++;
         }
     }
-    if($i > 0)echo "$i fájl feltöltve";
+    }
+    
     
     
     
@@ -39,6 +45,16 @@ if (isset($_FILES["fileToUpload"])) {
 
 <body>
 
+    <?php
+        if($i > 0)echo "$i fájl feltöltve";
+        if($errors){
+            foreach($errors as $error){
+                foreach($error as $errorMsg){
+                    echo "$errorMsg <br>";
+                }
+            }
+        }
+    ?>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         Select images to upload:
         <input type="file" name="fileToUpload[]" id="fileToUpload" multiple>
