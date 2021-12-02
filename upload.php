00f368b4d2154d643484b5cbe66e124f06b75e7d
Gombos János <54068067+gombosjanos@users.noreply.github.com>
2021-12-02 09:57:30 +0100

    uploads

---
+++ upload.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_FILES["fileToUpload"])) {
+    $target_dir = "uploads/";
+    echo $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file has been uploaded.";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+
+</body>
+
+</html>
