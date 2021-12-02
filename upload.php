b0b0a662c0e33d26d9624794a7b164432046ce02
Gombos János <54068067+gombosjanos@users.noreply.github.com>
2021-12-02 11:52:36 +0100

    FÁJL FELÖLTÉS MÉRET

--- upload.php
+++ upload.php
@@ -1,11 +1,35 @@
 <?php
+
+
+
 if (isset($_FILES["fileToUpload"])) {
-   
+    echo "<pre>";
+    print_r($_FILES["fileToUpload"]);
+    echo "</pre>";
     $target_dir = "uploads/";
-    echo $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file has been uploaded.";
+
+    $i=0;
+    $errirs = array();
+
+    foreach($_FILES["fileToUpload"]["name"] as $key => $name){
+         $target_file = $target_dir . basename($name);
+        
+         if($_FILES["fileToUpload"]["size"][$key] > 1024000){
+             $errors[0][] ="A $name TÚL NAGY";
+             
+         }
+         if($_FILES["fileToUpload"]["size"][$key] < 1024){
+            $errors[0][] ="A $name TÚL KICSI";
+            
+        }
+         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file)) {
+            $i++;
+        }
     }
+    if($i > 0)echo "$i fájl feltöltve";
+    
+    
+    
 
 }
 
@@ -17,8 +41,8 @@ if (isset($_FILES["fileToUpload"])) {
 
     <form action="upload.php" method="post" enctype="multipart/form-data">
         Select images to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload" multiple>
-        <input type="submit" value="Upload Image" name="submit">
+        <input type="file" name="fileToUpload[]" id="fileToUpload" multiple>
+        <input type="submit" value="Upload Images" name="submit">
     </form>
 
 </body>
