afa19c7b0d856af451c4aa41dba4bf53dc2a5901
Gombos János <54068067+gombosjanos@users.noreply.github.com>
2021-12-10 12:14:41 +0100

    telepulesek php

---
+++ telepulesek.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = "localhost";
+$username = "Jani";
+$password = "xldPl3vWywo0Clkg";
+$dbname = "teszt";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
+
+
+$file = "telepulesek.txt";
+$file_handle = fopen($file, "r");
+
+
+fclose($file_handle);
+
+
+
+?>
