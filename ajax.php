118639a046286cae7a814006929b6eb757721910
Gombos János <54068067+gombosjanos@users.noreply.github.com>
2022-02-10 10:59:54 +0100

    ajax 3.0

--- ajax.php
+++ ajax.php
@@ -4,7 +4,7 @@ require 'includes/db.inc.php';
 
 if(isset($_REQUEST['keres'])) {
    
-    $sql="SELECT nev FROM ulesrend WHERE nev LIKE '".$_REQUEST['keres']."%'";
+    $sql="SELECT nev FROM ulesrend WHERE nev LIKE '%".$_REQUEST['keres']."%'";
     if(!$result = $conn->query($sql))  echo $conn->error;
     if($result->num_rows > 0){
         while($row = $result->fetch_assoc()){
