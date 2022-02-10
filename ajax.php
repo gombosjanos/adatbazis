b3e97fdca3b81faa6b955f07f353c031df161aa6
Gombos János <54068067+gombosjanos@users.noreply.github.com>
2022-02-10 10:49:04 +0100

    ajax 2.0

--- ajax.php
+++ ajax.php
@@ -1,13 +1,17 @@
 <?php
 
-// keresés megvalósítása
-
 require 'includes/db.inc.php';
 
 if(isset($_REQUEST['keres'])) {
    
-   
-    // echo $_REQUEST['keres'];
+    $sql="SELECT nev FROM ulesrend WHERE nev LIKE '".$_REQUEST['keres']."%'";
+    if(!$result = $conn->query($sql))  echo $conn->error;
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            echo $row['nev']."<br>";
+        }
+    }else echo "Nincs ilyen név";
+    
 }
 
 
